c72ad00b68e2b1764280fa6c3d5f7ca0db1ff5d3
jesus <jesus.ramon2192@gmail.com>
2023-04-27 10:31:05 -0600

    Error de cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handle')
@@ -7,6 +8,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if ( whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 routerApi(app);
 
 app.use(logErrors);
